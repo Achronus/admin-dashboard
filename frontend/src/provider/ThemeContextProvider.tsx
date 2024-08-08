96257f4bcf413e9fcea6494d42d43cdd9559e4d0
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-08-08 21:03:50 +0100

    Add `ThemeContext` and apply to `RootLayout`

---
+++ frontend/src/provider/ThemeContextProvider.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { ThemeMode } from "@/lib/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createContext, useContext, useState } from "react";
+
+type ThemeContextProviderProps = {
+  theme: ThemeMode;
+  setTheme: (mode: ThemeMode) => void;
+};
+
+const ThemeContext = createContext<ThemeContextProviderProps | null>(null);
+
+type ThemeContextProps = {
+  children: React.ReactNode;
+};
+
+export const ThemeContextProvider = ({ children }: ThemeContextProps) => {
+  const [theme, setTheme] = useState<ThemeMode>("dark");
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext = (): ThemeContextProviderProps => {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error(
+      "useThemeContext must be used within a ThemeContextProvider"
+    );
+  }
+
+  return context;
+};
