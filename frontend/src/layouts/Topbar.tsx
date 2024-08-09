0e4a850c9885671daa6c03a215d0b62607d03e6e
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-08-09 12:02:37 +0100

    Add `Topbar` layout

--- frontend/src/layouts/Topbar.tsx
+++ frontend/src/layouts/Topbar.tsx
@@ -1,5 +1,52 @@
+"use client";
+
+import { tokens } from "@/lib/theme";
+import { useThemeContext } from "@/providers/ThemeContextProvider";
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import SearchIcon from "@mui/icons-material/Search";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
 const Topbar = () => {
-  return <div>Topbar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const { toggleTheme } = useThemeContext();
+
+  return (
+    <div className="flex justify-between p-4">
+      {/* Search Box */}
+      <Box display="flex" bgcolor={colors.primary[400]} borderRadius="3px">
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* Icons */}
+      <Box display="flex">
+        <IconButton onClick={toggleTheme}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsNoneIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlineIcon />
+        </IconButton>
+      </Box>
+    </div>
+  );
 };
 
 export default Topbar;
